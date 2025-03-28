6f7bb714a5bb4a8404e75d6b85d5d2b06d33fa41
Glitch (glitch-hello-website) <none>
2025-03-28 16:31:05 +0000

    🐾🌓 Checkpoint ./level2_a/style.css:76608169/213 ./level2_a/level2_a.html:76608169/28 ./level2_a/script.js:76608169/444

--- level2_a/script.js
+++ level2_a/script.js
@@ -4,5 +4,13 @@ document.body.onclick = function() {
   if(!clicked){
     clicked = !clicked;
   }
-  alert(clicked)
+  alert(clicked);
+  display_riddle();
   ;}
+function display_riddle(){
+  document.body.innerHTML = '';
+  var node = document.createElement("div");
+  node.setAttribute("id", "div1");  
+  var temp =  document.createTextNode("Water");
+  
+}
