59a9005488e9f5920320758ebe83cffed0e82010
Glitch (glitch-hello-website) <none>
2025-03-28 21:40:13 +0000

    🌋🏋 Checkpoint ./level2_a/script.js:76608169/24

--- level2_a/script.js
+++ level2_a/script.js
@@ -11,7 +11,7 @@ function display_riddle(){
   var node = document.createElement("div");
   node.setAttribute("id", "div1");
   node.setAttribute("class", "container");
-  node.innerHTML = "<p>LOCK</p>";
+  node.innerHTML = "<div></div>";
   document.body.append(node);
   
   alert(document.body.innerHTML);
