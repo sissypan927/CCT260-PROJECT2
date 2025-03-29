be9c7d2da0285a5097f44a546e20e9369d4617bb
Glitch (glitch-hello-website) <none>
2025-03-29 14:42:59 +0000

    🚿🛬 Checkpoint ./level2_a/script.js:76608169/946 ./level2_a/style.css:76608169/285

--- level2_a/script.js
+++ level2_a/script.js
@@ -11,13 +11,26 @@ document.body.onclick = function() {
 function display_riddle(){
   // var container = document.createElement("div");
   var container = document.createElement("div");
-  node2.setAttribute("id", "content");
+  container.setAttribute("id", "content");
   
   var node = document.createElement("div");
   var heading = document.createElement("h1");
   node.setAttribute("id", "riddle");
   heading.textContent = "Riddle:";
+  var riddle = document.createElement("p");
+  riddle.append("I am a five-digit number. The sum of my digits is 25, and exactly one digit repeats consecutively (like '22').");
+  riddle.append(document.createElement("br"))
+  riddle.append("My second digit is larger than both my first and third digits.");
+  riddle.append(document.createElement("br"))
+  riddle.append("My ten-thousands digit is 4, and my ones digit is 7. My hundreds digit is");
+  riddle.append(document.createElement("br"))
+  riddle.append("even and smaller than my tens digit. No digit appears more than twice in");
+  riddle.append(document.createElement("br"))
+  riddle.append("the number. What number am I?");
+  node.append(riddle);
+  
   node.append(heading);
+  node.append(riddle);
   container.append(node);
   
   
