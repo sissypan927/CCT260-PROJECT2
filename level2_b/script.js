84a01fb02e3a4c2f53d3228e7272958ddaf2294c
Glitch (glitch-hello-website) <none>
2025-03-29 18:15:40 +0000

    🏊🏼🚍 Checkpoint ./level2_b/script.js:76608169/1053 ./level2_b/style.css:76608169/189

--- level2_b/script.js
+++ level2_b/script.js
@@ -1,5 +1,10 @@
 var clicked = false;
 var slot1 = false;
+var slot2 = false;
+var slot3 = false;
+var slot4 = false;
+var slot = 0;
+var piece = 0;
 document.body.onclick = function () {
   if (!clicked) {
     clicked = !clicked;
@@ -17,6 +22,7 @@ function load_instructions() {
   var button = document.createElement("button");
   button.textContent = "Click to start the puzzle";
   button.addEventListener("click", load_puzzle);
+  button.addEventListener("click", load_puzzle);
   document.body.append(header);
   document.body.append(para);
   document.body.append(button);
@@ -31,6 +37,7 @@ function load_puzzle() {
     temp_button = document.createElement("button");
     temp_button.setAttribute("class", "puzzle-slot");
     temp_button.setAttribute("id", "puzzle-slot" + i);
+    temp_button.addEventListener("click", () => check_puzzle(0, i));
     temp_button.textContent = i;
     puzzle_board.append(temp_button);
     
@@ -39,8 +46,24 @@ function load_puzzle() {
       row = document.createElement("div");
     }
   }
+
   document.body.append(puzzle_board);
+  
+  var pieces  = document.createElement("div");
+  var row = document.createElement("div");
+  for(var i = 1; i < 5; i++){
+    temp_button = document.createElement("button");
+    temp_button.setAttribute("class", "puzzle-piece");
+    temp_button.setAttribute("id", "puzzle-piece" + i);
+    temp_button.textContent = i;
+    pieces.append(temp_button);
+  
+  }
+  document.body.append(pieces);
 }
-function check_puzzle(){
+function check_puzzle(sl){
+  if(slot != 0 && piece != 0){
+    
+  }
   
 }
