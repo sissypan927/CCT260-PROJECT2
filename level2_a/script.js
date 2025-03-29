6ac15a1fa6d12260dca5303dca821f49510afcbd
Glitch (glitch-hello-website) <none>
2025-03-29 01:21:18 +0000

    👻👚 Checkpoint ./level2_a/script.js:76608169/515 ./level2_a/style.css:76608169/539

--- level2_a/script.js
+++ level2_a/script.js
@@ -12,17 +12,25 @@ function display_riddle(){
   node.setAttribute("id", "div1");
   node.setAttribute("class", "container");
   node.innerHTML = "<div id = 'display'>hi</div>";
+  //Append number buttons
   for (let i = 1; i < 10; i++){
     var temp = document.createElement("button");
     temp.textContent = i;
     node.append(temp);
   }
+  //append clear button
   var temp = document.createElement("button");
-  temp.textContent = 0;
+  temp.textContent = "CLEAR";
   node.append(temp);
   temp = document.createElement("button");
-  temp.textContent = "DEL";
+  //append 0 button
+  temp.textContent = 0;
   node.append(temp);
+  //append ENTER button
+  temp = document.createElement("button");
+  temp.textContent = "ENTER";
+  node.append(temp);
+  
   document.body.append(node);
   alert(document.body.innerHTML);
 }
