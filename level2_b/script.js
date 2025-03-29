bc65a23358a1919c71b52e3eb6b389915e11f517
Glitch (glitch-hello-website) <none>
2025-03-29 17:28:31 +0000

    🎅🚃 Checkpoint ./level2_b/level2_b.html:76608169/18 ./level2_b/script.js:76608169/804 ./level2_b/style.css:76608169/2

--- level2_b/script.js
+++ level2_b/script.js
@@ -3,9 +3,21 @@ document.body.onclick = function () {
   if (!clicked) {
     clicked = !clicked;
     document.body.innerHTML = "";
-    load_puzzle();
+    load_instructions();
   }
 };
+function load_instructions(){
+  var header = document.createElement("h1");
+  header.textContent = "Instructions";
+  var para = document.createElement("p");
+  para.textContent = "Solve the jigsaw puzzle by clicking the piece then clicking the slot you want to place it in. You have 20 seconds."
+  var button = document.createElement("button");
+  button.textContent = "Click to start the puzzle";
+  button.addEventListener("click", load_puzzle);
+  document.body.append(header);
+  document.body.append(para);
+  document.body.append(button);
+}
 function load_puzzle(){
   var puzzle_board = document.createElement("div");
   var temp_button = document.createElement("button");
