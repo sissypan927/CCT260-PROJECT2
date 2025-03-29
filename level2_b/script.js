1f1969b491b46b2ba6de92d895b38cd0627c426d
Glitch (glitch-hello-website) <none>
2025-03-29 17:40:37 +0000

    🐦❤️ Checkpoint ./level2_b/script.js:76608169/2635 ./level2_b/style.css:76608169/196

--- level2_b/script.js
+++ level2_b/script.js
@@ -2,15 +2,17 @@ var clicked = false;
 document.body.onclick = function () {
   if (!clicked) {
     clicked = !clicked;
-    document.body.innerHTML = "";
     load_instructions();
   }
 };
-function load_instructions(){
+function load_instructions() {
+  document.body.innerHTML = "";
+  
   var header = document.createElement("h1");
   header.textContent = "Instructions";
   var para = document.createElement("p");
-  para.textContent = "Solve the jigsaw puzzle by clicking the piece then clicking the slot you want to place it in. You have 20 seconds."
+  para.textContent =
+    "Solve the jigsaw puzzle by clicking the piece then clicking the slot you want to place it in. You have 20 seconds.";
   var button = document.createElement("button");
   button.textContent = "Click to start the puzzle";
   button.addEventListener("click", load_puzzle);
@@ -18,10 +20,21 @@ function load_instructions(){
   document.body.append(para);
   document.body.append(button);
 }
-function load_puzzle(){
+function load_puzzle() {
+  document.body.innerHTML = "";
+
   var puzzle_board = document.createElement("div");
-  var temp_button = document.createElement("button");
-  temp_button.textContent = "hi";
-  puzzle_board.append(temp_button);
+  var temp_button;
+  var row = document.createElement("div");
+  for(var i = 1; i < 5; i++){
+    temp_button = document.createElement("button");
+    temp_button.setAttribute("class", "puzzle-slot")
+    temp_button.textContent = i;
+    puzzle_board.append(temp_button);
+    if(i == 2){
+      puzzle_board.append(row);
+      row = document.createElement("div");
+    }
+  }
   document.body.append(puzzle_board);
 }
